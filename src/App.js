dbafeee0654d8df07f83c63ea92ff6639708ae63
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-11-16 17:05:20 +0500

    pagination commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import PaginationComponent from "./components/PaginationComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <PaginationComponent />
+      </div>
+    </div>
+  );
+}
+
+export default App;
